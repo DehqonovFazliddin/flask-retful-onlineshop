36cc2115b0f5612eec38fc78d79791f15b7bb9ca
Fazliddin <fazliddindehqonov092@gmail.com>
2021-08-04 16:09:01 +0500

    patch func is working for products

--- api/product/single.py
+++ api/product/single.py
@@ -1,7 +1,25 @@
-from flask_restful import Resource
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_restful import Resource, reqparse, abort
 
+from models import Product
+from config import db
 
+parser = reqparse.RequestParser()
+parser.add_argument("name")
+parser.add_argument("price")
+parser.add_argument("stock")
+parser.add_argument("category")
 
 class SingleProductResource(Resource):
-    pass
+    def patch(self, pk):
+        args = parser.parse_args()
+        product = Product.query.filter_by(id=pk)
+        if not product.count() == 0:
+            product = product.first()
+            product.name = args["name"]
+            product.price = args["price"]
+            product.stock = args["stock"]
+            product.category = args["category"]
+            db.session.commit()
+            return product.serialize(), 201
+        else:
+            return abort(404, message="Requested product is not found.")
